7718496be0deeb3c32181c135a88f8165ae9c822
Rodrigo <rodriferant@gmail.com>
2022-08-11 18:55:34 -0400

    Casi listo para conectar BD

---
+++ src/main/java/com/sales/market/model/Employee.java
@@ -0,0 +1,53 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package com.sales.market.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Employee extends ModelBase {
+    private String firstName;
+    private String lastName;
+    private Byte[] image;
+
+    @OneToMany(mappedBy = "employee", fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+    private List<Contract> contracts;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public List<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<Contract> contracts) {
+        this.contracts = contracts;
+    }
+}
